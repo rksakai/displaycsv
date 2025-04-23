5db5780bbaf48c47b43f4e806de688bcec7c045d
renatonunes01 <rn783447@gmail.com>
2025-04-22 21:23:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def display_file():
     return render_template('display.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
