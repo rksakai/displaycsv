8625ba644672c16f6d34e7be61c7229e1c137e4f
renatonunes01 <rn783447@gmail.com>
2025-04-23 22:06:36 -0300

    Create AlertasUrbanosApplication.java
    Add main Spring Boot application class

---
+++ src/main/java/com/example/alertas/AlertasUrbanosApplication.java
@@ -0,0 +1,11 @@
+package com.example.alertas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertasUrbanosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlertasUrbanosApplication.class, args);
+    }
+}
