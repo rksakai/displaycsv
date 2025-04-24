7a60d364c90fde7ea3329f6218e7740322861114
renatonunes01 <rn783447@gmail.com>
2025-04-23 22:08:19 -0300

    Create AlertRepository.java
    Add AlertRepository interface

---
+++ src/main/java/com/example/alertas/src/main/java/com/example/alertas/model/src/main/java/com/example/alertas/repository/AlertRepository.java
@@ -0,0 +1,9 @@
+package com.example.alertas.repository;
+
+import com.example.alertas.model.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Long> {
+}
